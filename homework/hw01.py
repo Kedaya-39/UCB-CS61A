2aae26f76832ccdd8beeb6e39dc14f823a95a53a
Kedaya-39 <125474175+Kedaya-39@users.noreply.github.com>
2023-07-31 17:34:35 +0800

    Update hw01.py

--- homework/hw01.py
+++ homework/hw01.py
@@ -126,14 +126,14 @@ def hailstone(n):
     >>> a
     7
     """
-    "*** YOUR CODE HERE ***"
+    
     cnt = 0
     while True:
         print(n)
         cnt += 1
         if n == 1:
             return cnt
-        if n%2 ==0:
+        if n%2 == 0:
             n //= 2
         else:
             n = n*3 + 1
