29f0d0248ac50ac54678cb9c5f20b7f3ad89a14d
Kedaya-39 <125474175+Kedaya-39@users.noreply.github.com>
2023-08-11 18:12:52 +0800

    creat lab01

---
+++ lab/lab01/lab01.py
@@ -0,0 +1,71 @@
+def falling(n, k):
+    """Compute the falling factorial of n to depth k.
+
+    >>> falling(6, 3)  # 6 * 5 * 4
+    120
+    >>> falling(4, 3)  # 4 * 3 * 2
+    24
+    >>> falling(4, 1)  # 4
+    4
+    >>> falling(4, 0)
+    1
+    """
+    "*** YOUR CODE HERE ***"
+    rst = 1
+    if k <= 0:
+        return 1
+    for i in range(n, n-k, -1):
+        rst *= i
+    return rst    
+
+
+def sum_digits(y):
+    """Sum all the digits of y.
+
+    >>> sum_digits(10) # 1 + 0 = 1
+    1
+    >>> sum_digits(4224) # 4 + 2 + 2 + 4 = 12
+    12
+    >>> sum_digits(1234567890)
+    45
+    >>> a = sum_digits(123) # make sure that you are using return rather than print
+    >>> a
+    6
+    """
+    "*** YOUR CODE HERE ***"
+    d = 10
+    rst = 0
+    while y > 0:
+        rst += y % d
+        y //= d
+    return rst
+
+
+def double_eights(n):
+    """Return true if n has two eights in a row.
+    >>> double_eights(8)
+    False
+    >>> double_eights(88)
+    True
+    >>> double_eights(2882)
+    True
+    >>> double_eights(880088)
+    True
+    >>> double_eights(12345)
+    False
+    >>> double_eights(80808080)
+    False
+    """
+    "*** YOUR CODE HERE ***"
+    d = 10
+    cnt = 0
+    while n > 0:
+        x = n % d
+        if x == 8:
+            cnt +=1
+            if cnt == 2:
+                return True
+        else:
+            cnt = 0
+        n //= d
+    return False
